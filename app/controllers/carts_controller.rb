7d2a5051c4c1a09c07c13169fd5281ec68de50e4
cristianoprogramador <cristiano_own@hotmail.com.br>
2023-09-12 17:10:29 -0300

    feat: add the cart and user relations

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -5,4 +5,15 @@ class CartsController < ApplicationController
     @cart = current_user.cart
     @cart_items = @cart.cart_items.includes(:product)
   end
+
+  def create
+    @cart = current_user.cart || current_user.create_cart
+    @cart_item = @cart.cart_items.new(cart_item_params)
+    if @cart_item.save
+      redirect_to cart_path(@cart), notice: "Item adicionado ao carrinho!"
+    else
+      # handle the error case
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root 'home#index'
-  get 'products/:id', to: 'products#show', as: 'product'
+  resources :products, only: [:show]
 
   resources :carts, only: [:show]
   resources :cart_items, only: [:create, :update, :destroy]
