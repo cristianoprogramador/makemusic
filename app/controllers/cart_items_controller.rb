8948aa308409a3b5885b03319b4154264b16c105
cristianoprogramador <cristiano_own@hotmail.com.br>
2023-09-14 14:34:46 -0300

    feat: changing cart controller

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,8 +1,10 @@
 class CartItemsController < ApplicationController
   def create
-    @cart_item = current_user.cart.cart_items.new(cart_item_params)
+    cart = current_cart
+    @cart_item = cart.cart_items.new(cart_item_params)
+
     if @cart_item.save
-      redirect_to cart_path(current_user.cart), notice: "Item adicionado ao carrinho!"
+      redirect_to cart_path(cart), notice: "Item adicionado ao carrinho!"
     else
       redirect_to products_path, alert: "Erro ao adicionar ao carrinho."
     end
@@ -10,8 +12,9 @@ class CartItemsController < ApplicationController
 
   def update
     @cart_item = CartItem.find(params[:id])
+
     if @cart_item.update(cart_item_params)
-      redirect_to cart_path(current_user.cart)
+      redirect_to cart_path(current_cart)
     else
       render :edit
     end
@@ -20,7 +23,7 @@ class CartItemsController < ApplicationController
   def destroy
     @cart_item = CartItem.find(params[:id])
     @cart_item.destroy
-    redirect_to cart_path(current_user.cart)
+    redirect_to cart_path(current_cart)
   end
 
   private
@@ -29,4 +32,22 @@ class CartItemsController < ApplicationController
     params.require(:cart_item).permit(:product_id, :quantity)
   end
 
+  def current_cart
+    if current_user
+      current_user.cart
+    else
+      retrieve_cart_from_session
+    end
+  end
+
+  def retrieve_cart_from_session
+    cart = Cart.find_by(id: session[:cart_id])
+
+    if cart.nil?
+      cart = Cart.create
+      session[:cart_id] = cart.id
+    end
+
+    cart
+  end
 end
--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,18 +2,41 @@ class CartsController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @cart = current_user.cart
+    @cart = current_cart
     @cart_items = @cart.cart_items.includes(:product)
   end
 
   def create
-    @cart = current_user.cart || current_user.create_cart
-    @cart_item = @cart.cart_items.new(cart_item_params)
+    cart = current_cart
+    @cart_item = cart.cart_items.new(cart_item_params)
     if @cart_item.save
-      redirect_to cart_path(@cart), notice: "Item adicionado ao carrinho!"
+      redirect_to cart_path(cart), notice: "Item adicionado ao carrinho!"
     else
-      # handle the error case
+      # handle the error case, redirecione para onde você achar mais adequado
+      redirect_to root_path, alert: "Erro ao adicionar item ao carrinho."
     end
   end
 
+  private
+
+  def current_cart
+    if current_user
+      current_user.cart || current_user.create_cart
+    else
+      retrieve_cart_from_session
+    end
+  end
+
+  def retrieve_cart_from_session
+    cart = Cart.find_by(id: session[:cart_id])
+    if cart.nil?
+      cart = Cart.create
+      session[:cart_id] = cart.id
+    end
+    cart
+  end
+
+  def cart_item_params
+    params.require(:cart_item).permit(:product_id, :quantity)
+  end
 end
